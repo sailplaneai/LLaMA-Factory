d9c2784a3cdd1e02584b13a3f4b69ae6328801fa
Vladimir Araujo <43154161+vgaraujov@users.noreply.github.com>
2024-08-07 16:00:21 +0200

    update loader.py

--- src/llamafactory/data/loader.py
+++ src/llamafactory/data/loader.py
@@ -140,7 +140,7 @@ def _get_merged_dataset(
     model_args: "ModelArguments",
     data_args: "DataArguments",
     training_args: "Seq2SeqTrainingArguments",
-    stage: Literal["pt", "sft", "rm", "ppo", "kto"],
+    stage: Literal["pt", "sft", "rm", "ppo", "kto", "vm"],
 ) -> Optional[Union["Dataset", "IterableDataset"]]:
     if dataset_names is None:
         return None
@@ -159,7 +159,7 @@ def _get_preprocessed_dataset(
     dataset: Optional[Union["Dataset", "IterableDataset"]],
     data_args: "DataArguments",
     training_args: "Seq2SeqTrainingArguments",
-    stage: Literal["pt", "sft", "rm", "vm", "ppo", "kto"],
+    stage: Literal["pt", "sft", "rm", "ppo", "kto", "vm"],
     template: "Template",
     tokenizer: "PreTrainedTokenizer",
     processor: Optional["ProcessorMixin"] = None,
@@ -199,7 +199,7 @@ def get_dataset(
     model_args: "ModelArguments",
     data_args: "DataArguments",
     training_args: "Seq2SeqTrainingArguments",
-    stage: Literal["pt", "sft", "rm", "ppo", "kto"],
+    stage: Literal["pt", "sft", "rm", "ppo", "kto", "vm"],
     tokenizer: "PreTrainedTokenizer",
     processor: Optional["ProcessorMixin"] = None,
 ) -> "DatasetModule":
